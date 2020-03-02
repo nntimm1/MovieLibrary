28544ba347789e0a8042d4e667c081f66b07cc7e
unknown <hjimmy0803@gmail.com>
2020-03-02 12:04:27 -0600

    Seeded Database.

--- WebAPISample/Data/ApplicationContext.cs
+++ WebAPISample/Data/ApplicationContext.cs
@@ -10,9 +10,20 @@ namespace WebAPISample.Data
         {
 
         }
+        public DbSet<Movie> Movies { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Movie>()
+                .HasData(
+             new Models.Movie {MovieId = 1, Title = "The Departed", Genre = "Drama", Director = "Martin Scorsese" },
+             new Models.Movie {MovieId = 2, Title = "The Dark Knight", Genre = "Drama", Director = "Christopher Nolan" },
+             new Models.Movie {MovieId = 3, Title = "Inception", Genre = "Drama", Director = "Christopher Nolan" },
+             new Models.Movie {MovieId = 4, Title = "Pineapple Express", Genre = "Comedy", Director = "David Gordon Green" },
+             new Models.Movie {MovieId = 5, Title = "Die Hard", Genre = "Action", Director = "John McTiernan" });
+         
+       
             // Seed data - needs migration
             // modelBuilder.Entity<Movie>
             //  .HasData(
@@ -21,6 +32,6 @@ namespace WebAPISample.Data
             // View PlayerTracker project for example
         }
 
-        public DbSet<Movie> Movies { get; set; }
+       // public DbSet<Movie> Movies { get; set; }
     }
 }
--- WebAPISample/Models/Movie.cs
+++ WebAPISample/Models/Movie.cs
@@ -10,6 +10,7 @@ namespace WebAPISample.Models
         // We dont specify [Key] here becuase by using conventions it knows MovieId is the PK based off of its name.
         public int MovieId { get; set; }
         public string Title { get; set; }
+        public string Genre { get; set; }
         public string Director { get; set; }
     }
 }
