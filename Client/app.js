917fc5dbca23e9b0f8f2578b4c0e459b3bc26541
Nicolas Timm <nntimm1@gmail.com>
2020-03-03 16:28:11 -0600

    EOD push and pull git add .

--- Client/app.js
+++ Client/app.js
@@ -18,31 +18,92 @@
       error: function(jqXhr, textStatus, errorThrown) {
         console.log(errorThrown);
       }
+      e.processForm();
     });
 
     e.preventDefault();
+
   }
-
-  $(document).ready(function() {
-    var movie_data = "";
-    $.getJSON("https://localhost:44325/api/movie", function(data) {
-      $.each(data, function(key, value) {
-        movie_data +=
-          "<tr>" +
-          "<td>" +
-          value.title +
-          "</td>" +
-          "<td>" +
-          value.genre +
-          "</td>" +
-          "<td>" +
-          value.director +
-          "</td>" +
-          "</tr>";
+  (function($) {
+    function processForm(e) {
+      var dict = {
+        Title: this["title"].value,
+        Genre: this["genre"].value,
+        Director: this["director"].value
+      };
+      $.ajax({
+        url:
+          "https://localhost:44325/api/movie/" +
+          "/" +
+          Put() /* im not sure adding this put method is correct found online */,
+        datatype: "json",
+        type: "put",
+        contentType: "application/json",
+        data: JSON.stringify(dict),
+        success: function(data, textStatus, jQxhr) {
+          $("#response pre").html(data);
+        },
+        error: function(jqXhr, textStatus, errorThrown) {
+          console.log(errorThrown);
+        }
       });
-      $("#Results_Table").append(movie_data);
-    });
-  });
+      e.preventDefault();
+      e.processForm();
+    }
+    (function($) {
+      function processForm(e) {
+        var dict = {
+          Title: this["title"].value,
+          Genre: this["genre"].value,
+          Director: this["director"].value
+        };
 
-  $("#my-form").submit(processForm);
-})(jQuery);
+        $.ajax({
+          url:
+            "https://localhost:44325/api/movie" +
+            "/" +
+            get() /* im not sure adding this get method is correct found online */,
+          dataType: "json",
+          type: "get",
+          contentType: "application/json",
+          data: JSON.stringify(dict),
+          success: function(data, textStatus, jQxhr) {
+            $("#response pre").html(data);
+          },
+          error: function(jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+          }
+        });
+
+        e.preventDefault();
+        e.processForm();
+      }
+
+      $(document).ready(function() {
+        var movie_data = "";
+        $.getJSON("https://localhost:44325/api/movie", function(data) {
+          $.each(data, function(key, value) {
+            movie_data +=
+              "<tr>" +
+              "<td>" +
+              value.title +
+              "</td>" +
+              "<td>" +
+              value.genre +
+              "</td>" +
+              "<td>" +
+              value.director +
+              "</td>" +
+              "</tr>";
+          });
+          $("#Results_Table").append(movie_data);
+        });
+      });
+
+      $("#my-form").submit(processForm);
+      $("#my-form").search(processForm);
+      $("#my-form").update(processForm);
+      $("#my-form").get(processForm);
+    })(jQuery);
+  });
+});
