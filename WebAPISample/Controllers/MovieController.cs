97c34e4eab106c60658f5f9581c35ceaa509bfa7
Nicolas Timm <nntimm1@gmail.com>
2020-03-03 08:17:34 -0600

    pulling updated files and pushing frontend updates

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -18,41 +18,41 @@ namespace WebAPISample.Controllers
         {
             _context = context;
         }
-        // GET api/movie
+
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            // TODO Retrieve all movies from db logic
+
             return new string[] { "movie1 string", "movie2 string" };
         }
 
-        // GET api/movie/5
+
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            // TODO Retrieve movie by id from db logic
+
             return "value";
         }
 
-        // POST api/movie
+
         [HttpPost]
         public void Post([FromBody]Movie value)
         {
-            // TODO Create movie in db logic
+
         }
 
-        // PUT api/movie/5
+
         [HttpPut]
         public void Put(int id, [FromBody]string value)
         {
-            // TODO Update movie in db logic
+
         }
 
-        // TODO DELETE api/movie/5
+
         [HttpDelete]
         public void Delete(int id)
         {
-            // TODO Delete movie from db logic
+
         }
     }
 }
