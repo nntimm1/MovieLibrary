837eee5f8a9e53140ff64456446891b4a8b8e606
Nicolas Timm <nntimm1@gmail.com>
2020-03-04 08:08:21 -0600

    fixed get all movies button and page load issue table now loads all data

--- Client/app.js
+++ Client/app.js
@@ -6,75 +6,55 @@
       Director: this["director"].value
     };
 
+    $.ajax({
+      url: "https://localhost:44325/api/movie",
+      dataType: "json",
+      type: "post",
+      contentType: "application/json",
+      data: JSON.stringify(dict),
+      success: function(data, textStatus, jQxhr) {
+        $("#response pre").html(data);
+      },
+      error: function(jqXhr, textStatus, errorThrown) {
+        console.log(errorThrown);
+      }
+    });
 
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
-            type: 'post',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
-        
+    e.preventDefault();
+  }
 
-        e.preventDefault();
-    }
+  function processForm(e) {
+    $.ajax({
+      url: "https://localhost:44325/api/movie/movieId",
+      datatype: "json",
+      type: "put",
+      contentType: "application/json",
+      data: JSON.stringify(dict),
+      success: function(data, textStatus, jQxhr) {
+        $("#response pre").html(data);
+      },
+      error: function(jqXhr, textStatus, errorThrown) {
+        console.log(errorThrown);
+      }
+    });
 
-    function processForm( e )
-    {
-        var dict = {
-            Title : this["title"].value,
-            Genre : this['genre'].value,
-            Director : this["director"].value
-        };
+    e.processForm();
+  }
 
-        $.ajax({
-            url: 'https://localhost:44325/api/movie/movieId',
-            datatype: 'json',
-            type: 'put',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function (data, textStatus, jQxhr){
-                $('#response pre').html(data);
-            },
-            error: function (jqXhr, textStatus, errorThrown){
-                console.log(errorThrown);
-            }
-        });
-
-        e.processForm();
-    }
-
-      $(document).ready(function() {
-        var movie_data = "";
-        $.getJSON("https://localhost:44325/api/movie", function(data) {
-          $.each(data, function(key, value) {
-            movie_data +=
-              "<tr>" +
-              "<td>" +
-              value.title +
-              "</td>" +
-              "<td>" +
-              value.genre +
-              "</td>" +
-              "<td>" +
-              value.director +
-              "</td>" +
-              "</tr>";
-          });
-          $("#Results_Table").append(movie_data);
-        });
+  $(document).ready(function() {
+    var movie_data = "";
+    $.getJSON("https://localhost:44325/api/movie", function(data) {
+      $.each(data, function(key, value) {
+        movie_data += "<tr>";
+        movie_data += "<td>" + value.title + "</td>";
+        movie_data += "<td>" + value.genre + "</td>";
+        movie_data += "<td>" + value.director + "</td>";
+        movie_data += "</tr>";
       });
-
-      $("#my-form").submit(processForm);
-      $("#my-form").search(processForm);
-      $("#my-form").update(processForm);
-      $("#my-form").get(processForm);
-    })(jQuery);
+      $("#Results_Table").append(movie_data);
+    });
   });
-});
+
+  $("#my-form").submit(processForm);
+  $("#my-form").get(processForm);
+})(jQuery);
