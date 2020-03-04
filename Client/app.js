74ef55b8debfa862c41c3133e2b9e735a39baac9
unknown <hjimmy0803@gmail.com>
2020-03-04 11:46:57 -0600

    Changed the look of our app.js. Realized we were adding functions in the wrong area.

--- Client/app.js
+++ Client/app.js
@@ -24,55 +24,43 @@
 
         e.preventDefault();
     }
+    $("#my-form").submit(processForm);
+  })(jQuery);
 
-    function processForm( e )
-    {
-        var dict = {
-            Title : this["title"].value,
-            Genre : this['genre'].value,
-            Director : this["director"].value
-        };
+  function updateMovie(e) {
+    $.ajax({
+      url: "https://localhost:44325/api/movie",
+      datatype: "json",
+      type: "put",
+      contentType: "application/json",
+      success: function(data, textStatus, jQxhr) {
+        console.log(data);
+      },
+      error: function(jqXhr, textStatus, errorThrown) {
+        console.log(errorThrown);
+      }
+    });
 
-        $.ajax({
-            url: 'https://localhost:44325/api/movie/movieId',
-            datatype: 'json',
-            type: 'put',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function (data, textStatus, jQxhr){
-                $('#response pre').html(data);
-            },
-            error: function (jqXhr, textStatus, errorThrown){
-                console.log(errorThrown);
-            }
-        });
-
-        e.processForm();
-    }
+    e.preventDefault();
+  }
 
   $(document).ready(function() {
     var movie_data = "";
     $.getJSON("https://localhost:44325/api/movie", function(data) {
       $.each(data, function(key, value) {
-        movie_data +=
-          "<tr>" +
-          "<td>" +
-          value.title +
-          "</td>" +
-          "<td>" +
-          value.genre +
-          "</td>" +
-          "<td>" +
-          value.director +
-          "</td>" +
-          "</tr>";
+        movie_data += "<tr>";
+        movie_data += "<td>" + value.title + "</td>";
+        movie_data += "<td>" + value.genre + "</td>";
+        movie_data += "<td>" + value.director + "</td>";
+        movie_data += "<td>" + "<button type='update'>Update</button>" + "</td>"
+        movie_data += "</tr>";
       });
       $("#Results_Table").append(movie_data);
     });
   });
 
+ 
+  
+  
 
-    $('#my-form').submit( processForm );
-
-})(jQuery);
-
+   
